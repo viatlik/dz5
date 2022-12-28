8af2e609d61a0a14de91d830866d94b4e195400b
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 00:34:20 +0300

    третье задание

---
+++ dz1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = new int[5];
+int count = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999+1); 
+    Console.Write(array[i] + ", ");
+
+} 
+for (int i=0; i<array.Length; i++)
+{
+   if (array[i] % 2 == 0) count+=1;
+}
+Console.Write(count);
---
+++ dz2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[5];
+int summ = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10); 
+    Console.Write(array[i] + ", ");
+} 
+for (int i=0; i<array.Length; i++)
+{
+   if (i % 2 == 1)
+    summ = summ + array[i];
+}
+Console.Write($"Сумма элементов, стоящих на нечётных позициях -> {summ}");
