1a792142815a4afd7466f2fa1218e88be5dd43a8
vitalik <vitalik-s-hedgehog@yandex.ru>
2022-12-29 00:39:42 +0300

    задание

---
+++ dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] numbers = { 1.4, 2.2, 3.8, 4.1, 5.9 };
+double numMax = numbers[0];
+double numMin = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] >= numMax) numMax = numbers[i]; i++;
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] <= numMin) numMin = numbers[i]; i++;
+}
+
+Console.WriteLine($"разницa между максимальным и минимальным элементов массива -> {numMax - numMin}");
